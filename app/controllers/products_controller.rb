845398c34183f56c99bf1ef9798e778d48883ecf
jbunny13 <jnguyen72086@gmail.com>
2016-02-20 18:29:01 -0700

    Improve average review

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,11 +12,7 @@ class ProductsController < ApplicationController
   def show
     @reviews = Review.where(product_id: @product.id).order("created_at DESC")
 
-    if @reviews.blank?
-      @average_review = 0
-    else
-      @average_review = @reviews.average(:rating).round(2)
-    end
+    @average_review = @reviews.blank? ? 0 : @reviews.average(:rating).round(2)
   end
 
   def new
