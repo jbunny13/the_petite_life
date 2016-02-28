870e1717427cb8d3f5adeb4493242d10e5b99dd9
jbunny13 <jnguyen72086@gmail.com>
2016-02-27 17:11:30 -0700

    Include user name fields in model + devise registration views

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,11 +2,11 @@ class RegistrationsController < Devise::RegistrationsController
 private
 
   def sign_up_params
-    params.require(:user).permit( :email, :password, :password_confirmation , :role)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :role)
   end
 
   def account_update_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :current_password , :role , :name)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :role,)
   end
   
 end
