9b3a0ae6b64d0e22594844bc91132eafefd5a79d
jbunny13 <jnguyen72086@gmail.com>
2016-03-04 12:07:54 -0700

    No search results working in the view now

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,19 +2,11 @@ class SearchesController < ApplicationController
   respond_to :html, :json
 
   def index
-    # if params[:query].present?
-    #   @pg_search_documents = PgSearch.multisearch(params[:query])
-    #   respond_with(@pg_search_documents)
-    # else
-    #   @pg_search_documents = []
-    #   flash[:notice] = "Looks like your search returned no results!"
-    # end
-
-    if params[:query].nil?
-      flash[:notice] = "Looks like your search returned no results!"
-    else
+    if params[:query]
       @pg_search_documents = PgSearch.multisearch(params[:query])
       respond_with(@pg_search_documents)
+    else
+      @pg_search_documents = []
     end
   end
 end
