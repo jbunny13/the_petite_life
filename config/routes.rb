2df0b75f31479faea6ceeb90cfc2753b8944e4eb
jbunny13 <jnguyen72086@gmail.com>
2016-03-07 02:04:10 -0700

    Remove 'popular' placeholder from routes + layout header partial, add nav links to Articles, Products, Resources

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'registrations'}
   resources :products do
     resources :reviews, except: [:show, :index]
-    collection do
-    	get 'popular'
-    end
   end
   
   root 'products#index'
