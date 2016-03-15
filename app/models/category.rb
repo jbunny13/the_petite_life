f6082e61b7b83e3e601206101bcba48c2806b754
jbunny13 <jnguyen72086@gmail.com>
2016-03-15 00:09:05 -0600

    Add search functionality to new models + update search results view

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,7 @@ class Category < ActiveRecord::Base
   has_and_belongs_to_many :references
 
   validates :name, presence: true, length: { minimum: 5 }
+
+  include PgSearch
+  multisearchable against: :name
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :article, required: true
 
   validates :content, presence: true, length: { minimum: 5 }
+
+  include PgSearch
+  multisearchable against: :content
 end
