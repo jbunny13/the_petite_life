1ef7c99174ff224335608ca40f932809d00e66b0
jbunny13 <jnguyen72086@gmail.com>
2016-04-19 15:04:38 -0600

    Products + Articles: modify model to allow shorter names, modify thumbnail dimensions

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,7 @@ class Category < ActiveRecord::Base
   has_and_belongs_to_many :products
   has_and_belongs_to_many :references
 
-  validates :name, presence: true, length: { minimum: 5 }
+  validates :name, presence: true, length: { minimum: 3 }
   
   extend FriendlyId
   friendly_id :name, use: [:slugged, :history]
--- app/models/product.rb
+++ app/models/product.rb
@@ -9,12 +9,12 @@ class Product < ActiveRecord::Base
   scope :by_name, -> { order(name: :asc) }
   scope :average_rating, -> { includes(:reviews).sort_by(&:average_rating).reverse }
 
-  validates :name, presence: true, length: { minimum: 5 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 5 }
 
   include ValidateTag
   
-  has_attached_file :image, styles: { medium: "400x500>", thumb: "250x200>" }, default_url: "missing.png"
+  has_attached_file :image, styles: { medium: "400x500>", thumb: "250x200" }, default_url: "missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   
   include PgSearch
