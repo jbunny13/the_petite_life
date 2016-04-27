e5a762ffe1e2f55f3ebdfce3175e1f8394b4c071
jbunny13 <jnguyen72086@gmail.com>
2016-04-27 12:37:54 -0600

    Revert redirect upon sign in to default behavior

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,10 +23,6 @@ class ApplicationController < ActionController::Base
     products_path
   end
 
-  def after_sign_in_path_for(resource)
-    products_path
-  end
-
   def after_sign_out_path_for(resource)
     products_path
   end
